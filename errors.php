f890b737f494328bbd06e4d75c9c53948a3dfd85
Karl <kcayme@gmail.com>
2022-05-22 00:26:22 +0800

    hi it's me

--- errors.php
+++ errors.php
@@ -1,7 +1,10 @@
 <?php  if (count($errors) > 0) : ?>
   <div class="error">
   	<?php foreach ($errors as $error) : ?>
-  	  <p><?php echo $error ?></p>
+  	  <p><?php echo $error;
+			echo "test";
+		?></p>
+		
   	<?php endforeach ?>
   </div>
 <?php  endif ?>
