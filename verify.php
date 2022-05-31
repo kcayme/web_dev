530d6c83e621f41d103a00937dba9c1517a1877c
Karl <kcayme@gmail.com>
2022-05-31 22:03:07 +0800

    functional verifiy.php

--- verify.php
+++ verify.php
@@ -1,22 +1,31 @@
 <?php
     // verifies id number input if it is registered
-    include("server.php");
-
+    include('server.php');
     if (isset($_POST['s_submit'])) {
-        $id_number = $_POST["id"];
-
+        $IDnum = $_POST["id"];
         // check if input id number is registered in database
         $idnum_check_query = "SELECT * FROM info WHERE id_number='$IDnum' LIMIT 1";
         $result = mysqli_query($db, $idnum_check_query);
         $match = mysqli_fetch_assoc($result);
-        
         if ($match) { // if id number exists or has an id number match
             if ($match['id_number'] === $IDnum) {
-                array_push($errors, "ID number already exists");
+                echo "VALID ID";
+                $name = $match['name'];
+                $address = $match['address'];
+                $contact = $match['number'];
+                $email = $match['email'];
+                $prev_timein = $match['time_in'];
+                echo "<p>
+                <b>Name: </b>$name<br>
+                <b>Address: </b>$address<br>
+                <b>Contact: </b>$contact<br>
+                <b>Email: </b>$email<br>
+                <b>Previous Time In: </b>$prev_timein<br>
+                </p>";
             }
         }
         else{
-
+            echo "<p>ID Number not registered!</p>";
         }
     }
 ?>
