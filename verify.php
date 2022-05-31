4d172ba8a331555b0ca3d4a7be29d251456306f1
rzainm <raizsnicole@gmail.com>
2022-05-31 21:10:16 +0800

    rarw

--- verify.php
+++ verify.php
@@ -1,6 +1,7 @@
-// verifies id number input if it is registered
+
 <?php
-    include("server.php");
+    //verifies id number input if it is registered
+    //include("server.php");
     
     if (isset($_POST['s_submit'])) {
 
